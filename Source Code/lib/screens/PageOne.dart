902321e7285ea9904915ca2eabe830230f4d128a
Christopher Altounian <christopher.altounian.798@my.csun.edu>
2020-04-01 22:31:18 +0000

    Single Ingredient Page

---
+++ Source Code/lib/screens/PageOne.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/classes/AppBarConfig.dart';
+import 'package:food_app/classes/StarsRating.dart';
+
+class PageOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: heartButton,
+        drawer: Drawer(),
+        appBar: AppBarConfig(),
+        body: Column(
+          children: <Widget>[
+            FittedBox(
+              child: Image.asset(
+                'images/EggSalmon.jpg',
+                height: 500,
+                width: 500,
+                fit: BoxFit.fill,
+              ), // demo, replace with what user clicked on.
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                prepIcon,
+                cookIcon,
+                diffIcon,
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                name,
+                StarsRating(),
+                ingredients,
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*                  FUNCTIONS & VARIABLES BELOW                            */
+
+var name = Container(
+  child: Text(
+    'Salmon Egg Salad', // replace with actual name of dish
+    style: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+    ),
+  ),
+);
+
+var ingredients = Container(
+  child: Text(
+      '* Wild salmon fillet for 1 to 6 servings salt and pepper \n* Salt and ground pepper \n* Yadayadayadayada'),
+);
+
+var prepIcon = Container(
+  child: Column(
+    children: <Widget>[Icon(Icons.restaurant), Text('Prep Time:\n25 Min')],
+  ),
+);
+
+var cookIcon = Container(
+  child: Column(
+    children: <Widget>[Icon(Icons.timer), Text('Cook Time:\n15 Min')],
+  ),
+);
+
+var diffIcon = Container(
+    child: Column(
+  children: <Widget>[
+    Icon(Icons.accessibility),
+    Text('Difficulty Level:\nBeginner')
+  ],
+));
+
+var heartButton = Container(
+  child: FloatingActionButton(
+    backgroundColor: Colors.orange[400],
+    child: Icon(
+      Icons.favorite,
+      color: Colors.white,
+    ),
+    onPressed: null,
+  ),
+);
