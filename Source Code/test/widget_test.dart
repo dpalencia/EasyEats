8f716b255d524b82e94f7d1c16a9e021161e32d3
Daniel Palencia <dpalencia2112@gmail.com>
2020-04-09 16:59:18 -0700

    Update Unit Test

--- Source Code/test/widget_test.dart
+++ Source Code/test/widget_test.dart
@@ -5,26 +5,18 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:odysseusrecipes/main.dart';
+import '../lib/main.dart';
+import '../lib/screens/LoginScreen.dart';
+import '../lib/screens/Root.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.pumpWidget(LoginScreen(FirebaseAuth.instance));
+    expect(find.byType(ListView), findsOneWidget); // Check for a ListView Form
   });
 }
